2eede83fca254b3a20651e07b96743b500588dc4
Sterling <sterling.nesbitt@gmail.com>
2021-10-09 16:59:21 -0400

    can now return a sequence of news. specify start date and num of days

--- news.py
+++ news.py
@@ -1,25 +1,32 @@
 from GoogleNews import GoogleNews
+import datetime
+import numpy as np
 
-def get_news(search_term, date, field='title', num_searches=5):
+def get_news(date, search_term, field='title', num_searches=5):
     googlenews = GoogleNews(start=date, end=date)
     googlenews.search(search_term)
     results = googlenews.results()
-
-    #print('num results =', len(results))
-    # for i in range(len(results)):
-    #     print('SEARCH RESULT', i, ':')
-    #     for key in results[i]:
-    #         print(key, ':', results[i][key])
-
     strings = []
     for i in range(min(num_searches, len(results))):
         strings.append(results[i][field])
     return strings
 
+def get_news_seq(start_date, num_days, search_term, field='title', num_searches=1):
+    start_date = start_date.split('/')
+    day = int(start_date[0])
+    month = int(start_date[1])
+    year = int(start_date[2])
+    start_date = datetime.date(year=year, month=month, day=day)
+    date_list = []
+    for x in range(num_days):
+        date_list.append((start_date + datetime.timedelta(days=x)).strftime("%m/%d/%Y"))
+    news_list = []
+    for date in date_list:
+        news_list.append(' '.join(get_news(date, search_term, field, num_searches=num_searches)))
+    return np.array(news_list)
+
 if __name__ == '__main__':
-    search_term = 'AAPL'
-    date = '12/04/2019'
-    field = 'title'
-    list = get_news(search_term, date, field, num_searches=5)
-    for i, string in enumerate(list):
-        print(field, i, ':', string)
+    news_seq = get_news_seq(start_date='12/04/2019',
+                            num_days=10,
+                            search_term='AAPL')
+    print(news_seq)
